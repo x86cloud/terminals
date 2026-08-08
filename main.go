c93f957f7d8eb39f83e052f64c7c9cf21217b98b
zShiyang <791978082@qq.com>
2026-08-08 10:15:45 +0800

    feat: 调整 Windows 窗口顶栏与边框颜色与主题色一致

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 17, G: 20, B: 26, A: 1},
+		BackgroundColour: &options.RGBA{R: 238, G: 241, B: 246, A: 1},
 		OnStartup:        app.startup,
 		OnShutdown:       app.shutdown,
 		DragAndDrop: &options.DragAndDrop{
@@ -34,6 +34,21 @@ func main() {
 		Windows: &windows.Options{
 			WebviewIsTransparent: false,
 			WindowIsTranslucent:  false,
+			Theme:                windows.Light,
+			CustomTheme: &windows.ThemeSettings{
+				DarkModeTitleBar:           windows.RGB(238, 241, 246),
+				DarkModeTitleBarInactive:   windows.RGB(238, 241, 246),
+				DarkModeTitleText:          windows.RGB(31, 39, 51),
+				DarkModeTitleTextInactive:  windows.RGB(107, 118, 134),
+				DarkModeBorder:             windows.RGB(212, 219, 230),
+				DarkModeBorderInactive:     windows.RGB(212, 219, 230),
+				LightModeTitleBar:          windows.RGB(238, 241, 246),
+				LightModeTitleBarInactive:  windows.RGB(238, 241, 246),
+				LightModeTitleText:         windows.RGB(31, 39, 51),
+				LightModeTitleTextInactive: windows.RGB(107, 118, 134),
+				LightModeBorder:            windows.RGB(212, 219, 230),
+				LightModeBorderInactive:    windows.RGB(212, 219, 230),
+			},
 		},
 		Bind: []interface{}{
 			app,
